60a9f9dd5e3a2013c4c4c95db2bc78019bcfbf43
Romka Horokhov <7347253+NerveClasp@users.noreply.github.com>
2026-03-01 13:31:18 +0200

    deploy to gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: 'index.html'
-		})
-	}
+			pages: "build",
+			assets: "build",
+			fallback: "404.html",
+			precompress: false,
+			strict: true,
+		}),
+		paths: {
+			// Якщо твій репозиторій називається 'my-repo', то base має бути '/my-repo'
+			// Якщо це custom domain або username.github.io, залиш порожню стрічку
+			base: process.env.NODE_ENV === "production" ? "/tierduel" : "",
+		},
+	},
 };
 
 export default config;
